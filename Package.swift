945ee2af681198fcb028797bc93a0dd8733557b5
Mia Koring <koring.mia@gmail.com>
2025-10-29 23:48:41 +0100

    Moved to external package from BetterSync main repo

--- Package.swift
+++ Package.swift
@@ -1,22 +1,42 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import CompilerPluginSupport
 
 let package = Package(
     name: "BetterSyncSwiftUI",
+    platforms: [.macOS(.v13), .iOS(.v14), .tvOS(.v14), .macCatalyst(.v14), .visionOS(.v1)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "BetterSyncSwiftUI",
-            targets: ["BetterSyncSwiftUI"]
+            targets: ["BetterSyncSwiftUI", "BetterSyncMacros"]
         ),
+    ],
+    dependencies: [
+        //.package(url: "https://github.com/miakoring/BetterSync", branch: "main"),
+        .package(name: "BetterSync", path: "~/Documents/Fork/BetterSync"),
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.15.4"),
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "5.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", "600.0.0" ..< "601.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        
         .target(
-            name: "BetterSyncSwiftUI"
+            name: "BetterSyncSwiftUI",
+            dependencies: [
+                .byName(name: "BetterSync")
+            ]
+        ),
+        .macro(
+            name: "BetterSyncMacros",
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+            ]
         ),
         .testTarget(
             name: "BetterSyncSwiftUITests",
