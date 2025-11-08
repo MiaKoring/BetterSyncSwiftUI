de2637f7b74ee0ea475464eaf1260778cfe05b55
Mia Koring <koring.mia@gmail.com>
2025-11-08 14:40:27 +0100

    Made example working with swiftui after bettersync changes

---
+++ Examples/BasicExample/BasicExample/ContentView.swift
@@ -0,0 +1,105 @@
+//
+//  ContentView.swift
+//  BetterSyncSCUITest
+//
+//  Created by Mia Koring on 29.10.25.
+//
+import SwiftUI
+import BetterSync
+import BetterSyncSwiftUI
+
+struct ContentView: View {
+    @Query
+    var testItems: [Test]
+    @State var stop = false
+    @Environment(\.modelContext) var context
+    
+    init(predicate: Test._PredicateHelper = .init()) {
+        self._testItems = Query<Test>(predicate)
+    }
+    
+    var body: some View {
+        VStack {
+            Text("\(testItems.count)")
+            Button("generate") {
+                stop = false
+                Task {
+                    await context.updateAfterCompletion {
+                        for _ in 0...30 {
+                            if stop { return }
+                            do {
+                                try await context.insertInBackground(Test(flag: Int.random(in: 0...1) > 0, testEncryption: "\(Int.random(in: 0...1000))", randomValue: Int.random(in: 0...1000)))
+                            } catch {
+                                print(error.localizedDescription)
+                            }
+                        }
+                    }
+                }
+            }
+            Button("printQuery") { print(testItems.map { $0.id! }) }
+            Button("Stop") { stop = true }
+            Button("print managed instances") {
+                print(context.trackedObjectCount)
+            }
+            Button("add one") {
+                do {
+                    try context.insert(Test(flag: Int.random(in: 0...1) > 0, testEncryption: "\(Int.random(in: 0...1000))", randomValue: Int.random(in: 0...1000)))
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+            if let first = testItems.first {
+                ObservedTextField(item: first)
+            }
+            ScrollView {
+                ForEach(testItems) { item in
+                    TestModelDisplay(item: item)
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+struct ObservedTextField: View {
+    @ObservedObject var item: Test
+    
+    var body: some View {
+        Toggle("", isOn: item.$flag)
+        TextField("edit", text: item.$testEncryption.decrypted)
+        Picker("", selection: item.$selectedGroup) {
+            Text("none").tag(Optional<Group>.none)
+            ForEach(Group.allCases, id: \.rawValue) { group in
+                Text(group.rawValue)
+                    .tag(group)
+            }
+        }
+    }
+}
+
+struct TestModelDisplay: View {
+    @ObservedObject var item: Test
+    @Environment(\.modelContext) var context
+    var body: some View {
+        VStack {
+            HStack {
+                Toggle("", isOn: item.$flag)
+                Text("\(item.id ?? -1)")
+                Text(item.selectedGroup?.rawValue ?? "none")
+                Text(item.testEncryption.wrappedValue)
+                Text(item.randomValue.description)
+                Button("Delete") {
+                    do {
+                        try context.delete(item)
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+        }
+        .onAppear {
+            print("appeared \(item.id ?? -1)")
+        }
+    }
+}
+
--- Sources/BetterSyncSwiftUI/Model.swift
+++ Sources/BetterSyncSwiftUI/Model.swift
@@ -1,7 +1,7 @@
 import Combine
 import BetterSync
 
-@attached(member, names: named(init), named(id), named(setupFields), named(context), named(_getSchema), named(_fields), named(_fieldInformation), named(objectWillChange), named(_key), named(_PredicateHelper), named(_satisfiesConstraint))
+@attached(member, names: named(init), named(id), named(setupFields), named(context), named(_getSchema), named(_fields), named(_fieldInformation), named(objectWillChange), named(_key), named(_PredicateHelper), named(_satisfiesConstraint), named(notifyOfChanges))
 @attached(extension, conformances: PersistentModel, Sendable, ObservableObject)
 @attached(peer, names: arbitrary)
 public macro Model() = #externalMacro(
